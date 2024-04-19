9b72c6e0b41c1a0e1fcd99b130c6d2714568adee
Kalin Ivanov <kalin.at.ivanov@gmail.com>
2024-04-19 21:59:37 +0300

    Create LeapYEar.py

---
+++ Day_3/LeapYEar/LeapYEar.py
@@ -0,0 +1,9 @@
+# Which year do you want to check?
+year = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+if year % 4 = 0:
+    Print("Leap year")
+elif    
+
