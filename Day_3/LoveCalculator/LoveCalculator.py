c09e13933cc326613c6ad6efe700dc50e0f301c6
kivanov87 <kalin.at.ivanov@gmail.com>
2024-04-21 19:49:35 +0300

    Create LoveCalculator.py

---
+++ Day_3/LoveCalculator/LoveCalculator.py
@@ -0,0 +1,18 @@
+print("The Love Calculator is calculating your score...")
+name1 = input() # What is your name?
+name2 = input() # What is their name?
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+name = name1.lower() + name2.lower()
+
+true = name.count('t') + name.count('r') + name.count('u') + name.count('e')
+love = name.count('l') + name.count('o') + name.count('v') + name.count('e')
+
+score = int(str(true) + str(love))
+
+if (score < 10) or (score > 90):
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
